2058e9db92a5f28f2edda9898571238d9729eee1
kstsunhj <sunhuaijun123@gmail.com>
2022-01-27 16:31:58 +0800

    add addPoint.py & makeobj.py

--- makeObject.py
+++ makeObject.py
@@ -1,6 +1,8 @@
 import makeContour as mc
 import makeJson as mj
 import makeRhinoScript as ms
+import makeobj as mo
+import addPoint as ad
 import os
 
 folder = "chairs"
@@ -12,4 +14,6 @@ for path in os.listdir(folder):
         mc.run(os.path.join(folder, prefix))
         mj.run(os.path.join(folder, prefix))
         ms.run(os.path.join(folder, prefix))
+        mo.run(os.path.join(folder, prefix))
+        ad.run(os.path.join(folder, prefix))
         print(f"{prefix} is done!")
--- makeSingle.py
+++ makeSingle.py
@@ -1,9 +1,20 @@
 import makeContour as mc
 import makeJson as mj
 import makeRhinoScript as ms
+import makeobj as mo
+import addPoint as ad
 
-path = 'chairs\\61-1'
-
-mc.run(path)
-mj.run(path)
-ms.run(path)
+# for r in range(100):
+#     path = f'irregular_desk\\{r+1}-1'
+#     mc.run(path)
+#     mj.run(path)
+#     ms.run(path)
+#     mo.run(path)
+irr = [1]
+for r in irr:
+    path = f'irregular_desk\\{r}-1'
+    mc.run(path)
+    mj.run(path)
+    ms.run(path)
+    mo.run(path)
+    ad.run(path)
